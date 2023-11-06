85140f4f91d98330497e86558b86305103c8ca7d
amitkumar.niranjan <95871470+amitkumar-niranjan@users.noreply.github.com>
2023-11-06 08:19:20 +0000

    hoisting info updated

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,9 @@
-console.log("Amit Kumar Niranjan")
+console.log("Amit Kumar Niranjan") // for printing output js code output
+
+// Hoisting in JS
+/*
+* Hoisting is been worked using Var not with Let and Const
+* what is Hoisting? JS's engine 
+*
+*/
 
