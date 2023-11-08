5693f86f3aaa1de09afa54371d929e57561f10fd
[Amit Kumar] <[amitkumar.niranjan@htcinc.com]>
2023-11-08 11:56:23 +0530

    added new way of output as table

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,10 @@
+// https://tc39.es/ecma262/   original ECMA SCRIPT DOCUMENTATION
+// MDN IS FOR MOZILLA DOCUMENTATION
+
 const accountId = 100756342;
 let accountEmail = "amit@google.com";
-var accountPassword = "";
-
+var accountPassword = "12345";
+myName = " Amit Kumar Niranjan"
 // const likely means constant: not reinitialised
 
 // accountId = 7888; // we cannot reinitialised a const with another value it's like constant;
@@ -9,4 +12,8 @@ var accountPassword = "";
 
 accountEmail = "hello@google.com" // we can reinitialised with new value using let.
 console.log(accountEmail);
+/*
+Prefer not to use var in code in modern javascript
+*/
 
+console.table([accountEmail, accountPassword, myName]); // we can console o/p as table
--- 01_basics/splitStringAndAdd.js
+++ 01_basics/splitStringAndAdd.js
@@ -1,2 +1,5 @@
-let data = document.querySelector("div");
-console.log(data.textContent);
+let data1 = document.querySelector("h1");
+let data = document.getElementById("#num");
+
+let innerTxt = data1.textContent;
+console.log( typeof data, data, innerTxt);
