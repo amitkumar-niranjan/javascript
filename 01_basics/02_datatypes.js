2053e227b81bc1be9becaa827c379c8db89d63b8
[Amit Kumar] <[amitkumar.niranjan@htcinc.com]>
2023-11-08 14:37:52 +0530

    added datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+// there are two types of data types in JS
+// 1. Primitive 2. non Primitive
+// Examples:
+
+let name = "Amit"
+let age = 35
+let isLoggedIn = true
+let data = null // standard value
+let value = undefined
+//bigint
+// symbol -> unique
+// null -> standalone value its type is object
+
+// we can check the type of these variables using type of propertise of JS
+
+console.table([typeof name,typeof age,typeof isLoggedIn,typeof data,typeof value]);
+// String, number, boolean, object, undefined
+
