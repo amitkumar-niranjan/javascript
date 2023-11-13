bff7576934f0e758d0b3ed460a21061bdac0ad81
[Amit Kumar] <[amitkumar.niranjan@htcinc.com]>
2023-11-13 20:43:58 +0530

    unique element in side array using Set method solved

--- 01_basics/qDuplicateArrayToUnique.js
+++ 01_basics/qDuplicateArrayToUnique.js
@@ -1,26 +1,10 @@
 // write a code to eliminate duplicate array to unique
-// 1. bruteforce approach
+
 const arr = [1,2,3,4,4,5,5,6,7,6];
-const ansArr = [];
-let temp=0;
-let count = 0;
-for(let i=0;i<arr.length;i++){
-    for(let j=0;j<arr.length;j++){
-        if(arr[i]===arr[j]){
-            count++;
-            
-            if(count >= 2){
-                temp=arr[i];
-                arr.pop(arr[i])
-            }
 
-        } 
-        ansArr.push(temp);
-
-    }
-    // if(count <=1)
-    count =0;
-    // temp=-1;
+function removeDuplicates(params) {
+    return Array.from(new Set(params))
     
 }
+const ansArr = removeDuplicates(arr);
 console.log(arr, ansArr);
