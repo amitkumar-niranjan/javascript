5452b0f3fcc25440bcb6b3adada93473a889cc9d
[Amit Kumar] <[amitkumar.niranjan@htcinc.com]>
2023-11-17 18:32:50 +0530

    using map function

--- 01_basics/functionArrowfunction.js
+++ 01_basics/functionArrowfunction.js
@@ -10,9 +10,26 @@ const data = {
     age: 89,
     sex: "male",
     call: function() {
+        console.log(this); // here this is point to current object scope
         console.log(this.name); // here inside object "this keyword" inside function behaving like its scope
         return this.age
+    },
+    arrowfunction: ()=> {
+        console.log(this);
     },
 }
 
 console.log(data.call());
+console.log(data.arrowfunction());
+
+// fact = 6;
+// const factorial = (fact) =>{
+//     fact = fact * factorial(fact-1);
+// }
+// console.log(factorial(fact));
+const arr = [1,2,3,4,5]
+let tot=0;
+arr.map((v,i)=>{
+    tot=tot+v; 
+})
+console.log(tot);
