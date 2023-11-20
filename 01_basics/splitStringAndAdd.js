2bca50725773025a657bef60feba280c3d031609
[Amit Kumar] <[amitkumar.niranjan@htcinc.com]>
2023-11-20 15:27:31 +0530

    solved addition problem from html covertion to correct value

--- 01_basics/splitStringAndAdd.js
+++ 01_basics/splitStringAndAdd.js
@@ -3,3 +3,18 @@ data = data.textContent
 
 // let innerTxt = data1.textContent;
 console.log( typeof data, data)
+let arr = data.split("=")
+// console.log(arr[0].trim);
+arr[0] = arr[0].trim();
+
+console.log(arr[0]);
+
+let num = arr[0].split("+")
+console.log(num[0],num[1]);
+num[0] = Number(num[0]);
+num[1] = Number(num[1]);
+console.log(typeof num[0], typeof num[1],num[0],num[1]);
+arr[1]= num[0] +num[1]
+console.log(arr[1]);
+document.querySelector("h1").innerHTML = `${num[0]}+${num[1]}=${arr[1]}`
+// console.log(data);
